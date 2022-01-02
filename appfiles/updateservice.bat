a6ece6ec3261983bf8119b9797ec47d1c15eab25
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-01-02 16:35:54 +0100

    Add files via upload

---
+++ appfiles/updateservice.bat
@@ -0,0 +1,10 @@
+@echo off
+call internet.cmd https://github.com/rotvproHD/PAPER-TCLEANER/raw/main/appfiles/APPFILES.zip -saveTo C:\PAPERSTUDIOS\Temp\appupdate.zip
+for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
+@RD /S /Q C:\PAPERSTUDIOS\Data\%inproname%\version
+"archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\appupdate.zip" *.* C:\PAPERSTUDIOS\Data\%inproname%\
+cls
+echo.
+echo [!] FERTIG
+pause
+boot.bat
