66bd409afe8936c6ccb51c1ceffdf6616e0af7c6
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-01-04 10:47:27 +0100

    Update updateservice.bat

--- appfiles/updateservice.bat
+++ appfiles/updateservice.bat
@@ -1,6 +1,7 @@
 @echo off
 call internet.cmd https://github.com/rotvproHD/PAPER-TCLEANER/raw/main/appfiles/APPFILES.zip -saveTo C:\PAPERSTUDIOS\Temp\appupdate.zip
 for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
+del /q C:\PAPERSTUDIOS\Data\%inproname%\version\*
 rmdir /q C:\PAPERSTUDIOS\Data\%inproname%\version
 "archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\appupdate.zip" *.* C:\PAPERSTUDIOS\Data\%inproname%\
 cls
