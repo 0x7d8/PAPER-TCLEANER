b4bd144e2128cc6a8d84576250fec3d9d454e4a0
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-03-03 22:05:28 +0100

    Add files via upload

--- appfiles/updateservice.bat
+++ appfiles/updateservice.bat
@@ -1,11 +1,19 @@
 @echo off
+cls
+echo.
+echo  [i] DIE UPDATE DATEIEN WERDEN HERUNTERGELADEN,
+echo      BITTE WARTEN SIE EINEN MOMENT.
 call internet.cmd https://github.com/rotvproHD/PAPER-TCLEANER/raw/main/appfiles/APPFILES.zip -saveTo C:\PAPERSTUDIOS\Temp\appupdate.zip
 del /q C:\PAPERSTUDIOS\Data\%inproname%\versions\*
 rmdir /q C:\PAPERSTUDIOS\Data\%inproname%\versions
 for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
 "archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\appupdate.zip" *.* C:\PAPERSTUDIOS\Data\%inproname%\
+PING localhost -n 3 >NUL
 cls
 echo.
-echo [!] FERTIG
+echo  [i] DAS UPDATE WURDE INSTALLIERT,
+echo      HERZLICHEN GLUECKWUNSCH.
+echo.
 pause
+cls
 boot.bat
